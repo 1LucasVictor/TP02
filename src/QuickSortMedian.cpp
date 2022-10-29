4148641214ddfe294d1885c545bbf7fc3d6fb4fb
1LucasVictor <1victor.lucassilva@gmail.com>
2022-10-29 18:33:18 -0300

    Adjusting Algorithm

--- src/QuickSortMedian.cpp
+++ src/QuickSortMedian.cpp
@@ -4,12 +4,14 @@ int partitionM(int *v, int begin, int end) {
   int aux;
 
   // Getting the median value as pivot
-  if (v[begin] > v[pivotInd] && v[end] > v[begin]) {
+  if ((v[begin] > v[pivotInd] && v[end] > v[begin]) ||
+      (v[begin] < v[pivotInd] && v[begin] > v[end])) {
+
     aux = v[begin];
     v[begin] = v[pivotInd];
     v[pivotInd] = aux;
-  }
-  else if (v[end] > v[pivotInd] && v[begin] > v[end]) {
+  } else if ((v[end] > v[pivotInd] && v[begin] > v[end]) ||
+             (v[end] < v[pivotInd] && v[end] > v[begin])) {
     aux = v[end];
     v[end] = v[pivotInd];
     v[pivotInd] = aux;
